c7e5c3d03c6f74f398bf93e4bde7b6efbae0e6cf
nattanarcilla <115715124+nattanarcilla@users.noreply.github.com>
2022-11-03 09:45:54 +0800

    Update dailyfxrate-HKD.py

--- dailyfxrate-HKD.py
+++ dailyfxrate-HKD.py
@@ -1,10 +1,9 @@
-#import json
 import pandas as pd
 import datetime as dt
 from datetime import date, timedelta
 
 delta = dt.timedelta(days=1)
-i = date.today() - timedelta(days=5)
+i = date.today() - timedelta(days=2)
 ed = date.today() - timedelta(days=1)
 
 dates = []
@@ -18,11 +17,9 @@ webrate = pd.concat(pd.read_html(link+d)[0].assign(Date=d) for d in dates)
 filteredratexrate = webrate[webrate.Currency.isin(["AUD","CAD","CHF","EUR","GBP","INR","MXN","PHP","PEN","PLN","RMB","SGD","USD"])]
 fxrate = filteredratexrate[['Currency', 'Units per HKD', 'HKD per unit', 'Date']]
 
-#load current exchange rate file and merge with new exchange rate
-#oldfx = pd.DataFrame(json.load(open('dailyfxrate-HKD.json')))
 oldfx = pd.DataFrame(pd.read_csv('dailyfxrate-HKD.csv'))
 newfx = pd.DataFrame(fxrate)
 
 finalfx = pd.concat((oldfx, newfx ),ignore_index=True).drop_duplicates().reset_index(drop=True)
-#finalfx.to_json('./dailyfxrate-HKD.json', orient='records', indent=2)
+
 finalfx.to_csv('./dailyfxrate-HKD.csv', index=False)
