6cca08273a79935bd5ffa6ec4a769e82f2bba740
nattanarcilla <115715124+nattanarcilla@users.noreply.github.com>
2022-11-04 12:04:36 +0800

    Update monthlyfxrate-USD.py

--- monthlyfxrate-USD.py
+++ monthlyfxrate-USD.py
@@ -10,7 +10,7 @@ dates = pd.date_range(i, ed, freq='M').strftime("%Y-%m-%d")
 
 link = "https://www.xe.com/currencytables/?from=USD&date="
 fxrate = pd.concat(pd.read_html(link+d)[0].assign(Date=d) for d in dates)
-newfxrate = fxrate[fxrate.Currency.isin(["AUD","CAD","CHF","EUR","GBP","INR","MXN","PHP","PEN","PLN","RMB","SGD","HKD"])]
+newfxrate = fxrate[fxrate.Currency.isin(["AUD","CAD","CHF","EUR","GBP","INR","MXN","PHP","PEN","PLN","CNY","SGD","HKD"])]
 finalfx = newfxrate[['Currency', 'Units per USD', 'USD per unit', 'Date']]
 
 oldfx = pd.DataFrame(pd.read_csv('monthlyfxrate-USD.csv'))
