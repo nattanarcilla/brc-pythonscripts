b6e9a05797afa073a4410810f4a8a3ef0af21672
nattanarcilla <115715124+nattanarcilla@users.noreply.github.com>
2022-11-04 09:23:47 +0800

    Update dailyfxrate-HKD.py

--- dailyfxrate-HKD.py
+++ dailyfxrate-HKD.py
@@ -3,7 +3,7 @@ import datetime as dt
 from datetime import date, timedelta
 
 delta = dt.timedelta(days=1)
-i = date.today() - timedelta(days=2)
+i = date.today() - timedelta(days=3)
 ed = date.today()
 
 dates = []
@@ -14,7 +14,7 @@ while i <= ed:
 
 link = "https://www.xe.com/currencytables/?from=HKD&date="
 webrate = pd.concat(pd.read_html(link+d)[0].assign(Date=d) for d in dates)
-filteredratexrate = webrate[webrate.Currency.isin(["AUD","CAD","CHF","EUR","GBP","INR","MXN","PHP","PEN","PLN","RMB","SGD","USD"])]
+filteredratexrate = webrate[webrate.Currency.isin(["AUD","CAD","CHF","EUR","GBP","INR","MXN","PHP","PEN","PLN","CNY","SGD","USD"])]
 fxrate = filteredratexrate[['Currency', 'Units per HKD', 'HKD per unit', 'Date']]
 
 oldfx = pd.DataFrame(pd.read_csv('dailyfxrate-HKD.csv'))
