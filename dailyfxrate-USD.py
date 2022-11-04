d366ea1b0a20717c1411bc9d34acdc1512a6cbf8
nattanarcilla <115715124+nattanarcilla@users.noreply.github.com>
2022-11-04 09:24:18 +0800

    Update dailyfxrate-USD.py

--- dailyfxrate-USD.py
+++ dailyfxrate-USD.py
@@ -3,7 +3,7 @@ import datetime as dt
 from datetime import date, timedelta
 
 delta = dt.timedelta(days=1)
-i = date.today() - timedelta(days=2)
+i = date.today() - timedelta(days=3)
 ed = date.today()
 
 dates = []
@@ -14,7 +14,7 @@ while i <= ed:
 
 link = "https://www.xe.com/currencytables/?from=USD&date="
 webrate = pd.concat(pd.read_html(link+d)[0].assign(Date=d) for d in dates)
-filteredratexrate = webrate[webrate.Currency.isin(["AUD","CAD","CHF","EUR","GBP","INR","MXN","PHP","PEN","PLN","RMB","SGD","HKD"])]
+filteredratexrate = webrate[webrate.Currency.isin(["AUD","CAD","CHF","EUR","GBP","INR","MXN","PHP","PEN","PLN","CNY","SGD","HKD"])]
 fxrate = filteredratexrate[['Currency', 'Units per USD', 'USD per unit', 'Date']]
 
 oldfx = pd.DataFrame(pd.read_csv('dailyfxrate-USD.csv'))
