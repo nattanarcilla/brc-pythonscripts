47228761dd2dd259c64868a52d97d0919eedc5e9
nattanarcilla <115715124+nattanarcilla@users.noreply.github.com>
2022-11-09 22:03:01 +0800

    Update monthlyfxrate-HKD.py

--- monthlyfxrate-HKD.py
+++ monthlyfxrate-HKD.py
@@ -4,7 +4,7 @@ from datetime import date, timedelta
 from pandas.tseries.offsets import MonthEnd
 
 i = date.today() - timedelta(days=15)
-ed = date.today() - timedelta(days=1)
+ed = date.today() + timedelta(days=1)
 
 dates = pd.date_range(i, ed, freq='M').strftime("%Y-%m-%d")
 
@@ -13,8 +13,8 @@ fxrate = pd.concat(pd.read_html(link+d)[0].assign(Date=d) for d in dates)
 newfxrate = fxrate[fxrate.Currency.isin(["AUD","CAD","CHF","EUR","GBP","INR","MXN","PHP","PEN","PLN","CNY","SGD","USD"])]
 finalfx = newfxrate[['Currency', 'Units per HKD', 'HKD per unit', 'Date']]
 
-oldfx = pd.DataFrame(pd.read_csv('monthlyfxrate-HKD.csv'))
-newfx = pd.DataFrame(finalfx)
+oldfx = pd.DataFrame(pd.read_csv('monthlyfxrate-HKD.csv')).round({"Units per HKD":3, "HKD per unit":3})
+newfx = pd.DataFrame(finalfx).round({"Units per HKD":3, "HKD per unit":3})
 
 finalfx2 = pd.concat((oldfx, newfx ),ignore_index=True).drop_duplicates().reset_index(drop=True)
 finalfx2.to_csv('./monthlyfxrate-HKD.csv', index=False)
