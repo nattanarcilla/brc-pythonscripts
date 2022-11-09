a7a3831eafca0de9394ab41c82f9dc4d254da74b
nattanarcilla <115715124+nattanarcilla@users.noreply.github.com>
2022-11-09 21:55:16 +0800

    Update dailyfxrate-USD.py

--- dailyfxrate-USD.py
+++ dailyfxrate-USD.py
@@ -17,8 +17,8 @@ webrate = pd.concat(pd.read_html(link+d)[0].assign(Date=d) for d in dates)
 filteredratexrate = webrate[webrate.Currency.isin(["AUD","CAD","CHF","EUR","GBP","INR","MXN","PHP","PEN","PLN","CNY","SGD","HKD"])]
 fxrate = filteredratexrate[['Currency', 'Units per USD', 'USD per unit', 'Date']]
 
-oldfx = pd.DataFrame(pd.read_csv('dailyfxrate-USD.csv'))
-newfx = pd.DataFrame(fxrate)
+oldfx = pd.DataFrame(pd.read_csv('dailyfxrate-USD.csv')).round({"Units per USD":3, "USD per unit":3})
+newfx = pd.DataFrame(fxrate).round({"Units per USD":3, "USD per unit":3})
 
 finalfx = pd.concat((oldfx, newfx ),ignore_index=True).drop_duplicates().reset_index(drop=True)
 
