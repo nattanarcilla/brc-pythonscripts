c5b03c098a93683fa04db15cc0c81e5e9fec0521
nattanarcilla <115715124+nattanarcilla@users.noreply.github.com>
2022-11-09 21:54:19 +0800

    Update dailyfxrate-HKD.py

--- dailyfxrate-HKD.py
+++ dailyfxrate-HKD.py
@@ -17,8 +17,8 @@ webrate = pd.concat(pd.read_html(link+d)[0].assign(Date=d) for d in dates)
 filteredratexrate = webrate[webrate.Currency.isin(["AUD","CAD","CHF","EUR","GBP","INR","MXN","PHP","PEN","PLN","CNY","SGD","USD"])]
 fxrate = filteredratexrate[['Currency', 'Units per HKD', 'HKD per unit', 'Date']]
 
-oldfx = pd.DataFrame(pd.read_csv('dailyfxrate-HKD.csv'))
-newfx = pd.DataFrame(fxrate)
+oldfx = pd.DataFrame(pd.read_csv('dailyfxrate-HKD.csv')).round({"Units per HKD":3, "HKD per unit":3})
+newfx = pd.DataFrame(fxrate).round({"Units per HKD":3, "HKD per unit":3})
 
 finalfx = pd.concat((oldfx, newfx ),ignore_index=True).drop_duplicates().reset_index(drop=True)
 
