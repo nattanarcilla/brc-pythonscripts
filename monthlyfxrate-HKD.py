c68cc38d19c25f31d29eb085a61af706dcfb702c
nattanarcilla <115715124+nattanarcilla@users.noreply.github.com>
2022-12-01 08:53:19 +0800

    Update monthlyfxrate-HKD.py

--- monthlyfxrate-HKD.py
+++ monthlyfxrate-HKD.py
@@ -3,14 +3,14 @@ import datetime as dt
 from datetime import date, timedelta
 from pandas.tseries.offsets import MonthEnd
 
-i = date.today() - timedelta(days=15)
-ed = date.today()
+i = date.today() - timedelta(days=30)
+ed = date.today() + timedelta(days=3)
 
 dates = pd.date_range(i, ed, freq='M').strftime("%Y-%m-%d")
 
 link = "https://www.xe.com/currencytables/?from=HKD&date="
 fxrate = pd.concat(pd.read_html(link+d)[0].assign(Date=d) for d in dates)
-newfxrate = fxrate[fxrate.Currency.isin(["AUD","CAD","CHF","EUR","GBP","INR","MXN","PHP","PEN","PLN","CNY","SGD","NZD","USD"])]
+newfxrate = fxrate[fxrate.Currency.isin(["AUD","CAD","CHF","EUR","GBP","INR","MXN","PHP","PEN","PLN","CNY","SGD","NZD","HKD"])]
 finalfx = newfxrate[['Currency', 'Units per HKD', 'HKD per unit', 'Date']]
 
 oldfx = pd.DataFrame(pd.read_csv('monthlyfxrate-HKD.csv')).round({"Units per HKD":3, "HKD per unit":3})
